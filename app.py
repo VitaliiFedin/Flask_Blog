f75c9844d7261e362d4d77a5662088cf6e039857
VitaliiFedin <vitalii.fedinn@gmail.com>
2022-06-21 15:03:44 +0300

    update delete posts

--- app.py
+++ app.py
@@ -35,10 +35,22 @@ def posts():
     articles = Article.query.order_by(Article.date.desc()).all()
     return render_template("posts.html", articles=articles)
 
+
 @app.route('/posts/<int:id>')
 def post_details(id):
     article = Article.query.get(id)
     return render_template("post_details.html", article=article)
+
+
+@app.route('/posts/<int:id>/delete')
+def post_delete(id):
+    article = Article.query.get_or_404(id)
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect('/posts')
+    except:
+        return "An error occurred during deleting the post"
 
 
 @app.route('/create-article', methods=['POST', 'GET'])
@@ -58,6 +70,22 @@ def create_article():
             return redirect('/posts')
         except:
             return "An Error occurred during adding your article"
+
+
+@app.route('/posts/<int:id>/update', methods=['POST', 'GET'])
+def article_update(id):
+    article = Article.query.get(id)
+    if request.method == 'POST':
+        article.title = request.form['title']
+        article.intro = request.form['intro']
+        article.text = request.form['text']
+        try:
+            db.session.commit()
+            return redirect('/posts')
+        except:
+            return "An Error occurred during updating your article"
+    else:
+        return render_template("post-update.html", article=article)
 
 
 if __name__ == "__main__":
